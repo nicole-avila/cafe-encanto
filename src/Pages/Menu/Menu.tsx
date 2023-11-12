7f5bfd2f7cba19aa13449f4c1ef0672b72db632b
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2023-11-12 23:35:52 +0100

    Adapt Menu + display titles dynamically from data

--- src/Pages/Menu/Menu.tsx
+++ src/Pages/Menu/Menu.tsx
@@ -1,9 +1,14 @@
 // import "./Menu.scss";
+import Navbar from "../../components/Navbar/Navbar";
+import Product from "../../components/Product/Product";
+import { data } from "../../data";
 
 export default function Menu() {
   return (
     <div className="menu">
+      <Navbar />
       <h1>Menu</h1>
+      <Product products={data} />
     </div>
   );
 }
