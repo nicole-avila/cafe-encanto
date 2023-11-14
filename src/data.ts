0a719e0b2e8648c630319826b424e046343781f7
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2023-11-14 11:30:39 +0100

    Updated data - added category to all

--- src/data.ts
+++ src/data.ts
@@ -4,6 +4,7 @@ export const data = [
     title: "Kaffe",
     description:
       "Svart kaffe är så enkelt som det kan bli med malda kaffebönor bryggda i hett vatten, serverade varma. Och om du vill låta det låta lite fint kan du kalla svart kaffe vid dess riktiga namn: café noir.",
+    category: "Coffee",
     ingredients: ["Kaffe"],
     price: 25.0,
     image:
@@ -14,6 +15,7 @@ export const data = [
     title: "Espresso",
     description:
       "Espresso är ett koncentrerat kaffe bryggt genom att tvinga en liten mängd nästan kokande vatten genom fint malda kaffebönor. Det är den renaste kaffeupplevelsen.",
+    category: "Coffee",
     ingredients: ["Kaffe"],
     price: 30.0,
     image:
@@ -24,6 +26,7 @@ export const data = [
     title: "Cappuccino",
     description:
       "Cappuccino är en kaffedryck som består av en espresso, varm mjölk och ett generöst lager av skummad mjölk. Det är en härlig balans mellan starkt och krämigt.",
+    category: "Coffee",
     ingredients: ["Espresso", "Mjölk", "Skum"],
     price: 45.0,
     image:
@@ -34,6 +37,7 @@ export const data = [
     title: "Latte",
     description:
       "Latte är en mjuk och len kaffedryck gjord med en enkel espresso och ångad mjölk. En perfekt balans mellan styrka och krämighet.",
+    category: "Coffee",
     ingredients: ["Espresso", "Mjölk"],
     price: 40.0,
     image: "URL_HÄR",
@@ -43,6 +47,7 @@ export const data = [
     title: "Mocha",
     description:
       "Mocha kombinerar rik choklad och espresso, toppat med vispad grädde. En sötfest för dina smaklökar.",
+    category: "Coffee",
     ingredients: ["Espresso", "Choklad", "Grädde"],
     price: 50.0,
     image: "URL_HÄR",
@@ -52,6 +57,7 @@ export const data = [
     title: "Americano",
     description:
       "Americano är en ljus och uppfriskande kaffedryck, skapad genom att späda espresso med varmt vatten. Perfekt för den som gillar en mildare smak.",
+    category: "Coffee",
     ingredients: ["Espresso", "Vatten"],
     price: 35.0,
     image: "URL_HÄR",
@@ -61,6 +67,7 @@ export const data = [
     title: "Caramel Macchiato",
     description:
       "Caramel Macchiato är en söt och karamellig kaffedryck med espresso, mjölk och karamellsås. En underbar balans mellan sött och bittert.",
+    category: "Coffee",
     ingredients: ["Espresso", "Mjölk", "Karamellsås"],
     price: 55.0,
     image: "URL_HÄR",
@@ -70,6 +77,7 @@ export const data = [
     title: "Affogato",
     description:
       "Affogato är en enkel men ljuvlig dessertkaffe. En scoop vaniljglass överöses med en het espresso, skapar en smältande och god kombination.",
+    category: "Coffee",
     ingredients: ["Espresso", "Vaniljglass"],
     price: 60.0,
     image: "URL_HÄR",
@@ -79,6 +87,7 @@ export const data = [
     title: "Flat White",
     description:
       "Flat White är en krämig kaffedryck med två espresso-skott och ångad mjölk. En stark och len smakupplevelse.",
+    category: "Coffee",
     ingredients: ["Espresso", "Mjölk"],
     price: 45.0,
     image: "URL_HÄR",
@@ -88,6 +97,7 @@ export const data = [
     title: "Iced Coffee",
     description:
       "Iced Coffee är den perfekta svalkande drycken, med kaffe bryggt över is och sötat efter eget tycke. En uppfriskande njutning.",
+    category: "Coffee",
     ingredients: ["Kaffe", "Is"],
     price: 40.0,
     image: "URL_HÄR",
@@ -97,6 +107,7 @@ export const data = [
     title: "Irish Coffee",
     description:
       "Irish Coffee kombinerar kaffe, irländsk whiskey och vispad grädde. En värmande och uppiggande dryck.",
+    category: "Coffee",
     ingredients: ["Kaffe", "Irish Whiskey", "Grädde"],
     price: 55.0,
     image: "URL_HÄR",
@@ -106,6 +117,7 @@ export const data = [
     title: "Turkish Coffee",
     description:
       "Turkish Coffee är en traditionell och kraftfull kaffedryck bryggd med finmalet kaffe och kryddor. En rik smakresa.",
+    category: "Coffee",
     ingredients: ["Kaffe", "Kryddor"],
     price: 50.0,
     image: "URL_HÄR",
@@ -115,6 +127,7 @@ export const data = [
     title: "Ristretto",
     description:
       "Ristretto är en koncentrerad espresso med intensiv smak. Perfekt för den som vill ha en kraftfull kaffesmäll i en liten klunk.",
+    category: "Coffee",
     ingredients: ["Kaffe"],
     price: 30.0,
     image: "URL_HÄR",
@@ -124,6 +137,7 @@ export const data = [
     title: "Cortado",
     description:
       "Cortado är en kort och intensiv kaffedryck, skapad genom att blanda espresso och varm mjölk. En liten men kraftfull smakupplevelse.",
+    category: "Coffee",
     ingredients: ["Espresso", "Mjölk"],
     price: 35.0,
     image: "",
@@ -133,6 +147,7 @@ export const data = [
     title: "Varm Choklad",
     description:
       "Krämig chokladdryck toppad med vispad grädde och chokladströssel. En söt och tröstande favorit.",
+    category: "Chocolate",
     ingredients: ["Chokladpulver", "Mjölk", "Grädde", "Chokladströssel"],
     price: 45.0,
     image: "URL_HÄR",
@@ -142,6 +157,7 @@ export const data = [
     title: "Grönt Te Citrus",
     description:
       "Uppfriskande grönt te med en twist av fräsch citrussmak. Ett hälsosamt och uppiggande alternativ.",
+    category: "Tea",
     ingredients: ["Grönt te", "Citronskal", "Apelsinskal", "Honung"],
     price: 40.0,
     image: "URL_HÄR",
@@ -151,6 +167,7 @@ export const data = [
     title: "Pepparmint Tea",
     description:
       "Ett lugnande och uppfriskande örtte med en touch av pepparmint. Perfekt för avkoppling.",
+    category: "Tea",
     ingredients: ["Pepparmynta", "Hett vatten", "Honung (valfritt)"],
     price: 35.0,
     image: "URL_HÄR",
@@ -160,6 +177,7 @@ export const data = [
     title: "Fruktsmoothie",
     description:
       "En färgglad blandning av färska frukter, yoghurt och is. En svalkande och näringsrik dryck.",
+    category: "Smoothie",
     ingredients: ["Jordgubbar", "Banan", "Yoghurt", "Is"],
     price: 50.0,
     image: "URL_HÄR",
@@ -169,6 +187,7 @@ export const data = [
     title: "Berry Bliss",
     description:
       "En fruktig explosion av smaker med blandade bär, yoghurt och is. En läcker och energigivande smoothie.",
+    category: "Smoothie",
     ingredients: ["Hallon", "Blåbär", "Jordgubbar", "Yoghurt", "Is"],
     price: 55.0,
     image: "URL_HÄR",
@@ -178,6 +197,7 @@ export const data = [
     title: "Tropical Tango",
     description:
       "En tropisk dans av smaker med ananas, mango, banan, kokosmjölk och is. En resa till paradiset i varje klunk.",
+    category: "Smoothie",
     ingredients: ["Ananas", "Mango", "Banan", "Kokosmjölk", "Is"],
     price: 60.0,
     image: "URL_HÄR",
@@ -187,6 +207,7 @@ export const data = [
     title: "Citrus Burst",
     description:
       "En uppfriskande citruskicks med apelsin, grapefrukt, citron, yoghurt och is. Perfekt för en vitaminboost.",
+    category: "Smoothie",
     ingredients: ["Apelsin", "Grapefrukt", "Citron", "Yoghurt", "Is"],
     price: 50.0,
     image: "URL_HÄR",
@@ -196,6 +217,7 @@ export const data = [
     title: "Mango Madness",
     description:
       "En galen mix av mogna mangobitar, passionsfrukt, vaniljyoghurt och is. En tropisk fest i varje klunk.",
+    category: "Smoothie",
     ingredients: ["Mango", "Passionsfrukt", "Vaniljyoghurt", "Is"],
     price: 55.0,
     image: "URL_HÄR",
@@ -205,6 +227,7 @@ export const data = [
     title: "Green Goddess",
     description:
       "En hälsosam grönsakssmoothie med spenat, äpple, selleri, banan, och ingefära. Ett grönt energipaket.",
+    category: "Smoothie",
     ingredients: ["Spenat", "Äpple", "Selleri", "Banan", "Ingefära"],
     price: 45.0,
     image: "URL_HÄR",
